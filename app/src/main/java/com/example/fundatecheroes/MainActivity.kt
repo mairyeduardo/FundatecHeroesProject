aa16a1e3a02a3b5d4948c7eb1c5c32812949640d
mairyeduardo <mairyeduardo@gmail.com>
2023-08-24 01:17:57 -0300

    alteracoes

--- app/src/main/java/com/example/fundatecheroes/MainActivity.kt
+++ app/src/main/java/com/example/fundatecheroes/MainActivity.kt
@@ -7,5 +7,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val test = Test(
+        label1 = "Usuario",
+    )
+
+
     }
+
+  private fun executaAcao1(value1: String, value2: String){
+
+  }
+
+    private fun executaAcao2(value1: String, value2: String){
+
+    }
+
 }
+
+data class Test (val label1: String, val label2: String? = null)
