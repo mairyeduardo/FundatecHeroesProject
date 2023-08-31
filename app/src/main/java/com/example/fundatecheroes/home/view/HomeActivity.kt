4db4205e0ea4521946fd5c8b004fe93e7cbfc857
mairyeduardo <mairyeduardo@gmail.com>
2023-08-31 01:02:56 -0300

    primeiras alteracoes do projeto

---
+++ app/src/main/java/com/example/fundatecheroes/home/view/HomeActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fundatecheroes.home.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.fundatecheroes.R
+import com.example.fundatecheroes.gone
+import com.example.fundatecheroes.home.presentation.model.HomeViewState
+
+class HomeActivity : AppCompatActivity() {
+    private val button by lazy {
+        findViewById<Button>(R.id.button)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        observerState(HomeViewState.HideButton)
+    }
+
+    private fun observerState(state: HomeViewState){
+        when(state){
+            is HomeViewState.Sucess -> {
+                state.message
+            }
+            is HomeViewState.Error -> {
+                state.errorMessage
+            }
+            HomeViewState.Loading -> {
+
+            }
+            HomeViewState.HideButton -> {
+                button.gone()
+            }
+
+        }
+    }
+
+}
+
