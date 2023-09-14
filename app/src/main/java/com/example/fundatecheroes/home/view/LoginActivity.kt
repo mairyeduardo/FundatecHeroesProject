7090547b63de78c54c60511666461065e1c17d7a
Mairy Eduardo <mairyeduardo@gmail.com>
2023-09-13 23:04:20 -0300

    alternador de páginas e pequenas mudanças de tela do login

--- app/src/main/java/com/example/fundatecheroes/home/view/LoginActivity.kt
+++ app/src/main/java/com/example/fundatecheroes/home/view/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.fundatecheroes.home.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fundatecheroes.R
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,15 +16,16 @@ class LoginActivity : AppCompatActivity() {
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_login)
 
+        findViewById<TextView>(R.id.usuario_novo).setOnClickListener{
+            val intent = Intent(this@LoginActivity, ProfileActivity::class.java )
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.buttonLogin).setOnClickListener {
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
-
-    private fun irParaHomePage() {
-        val intent = Intent(this, HomeActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-
-
-
 }
+
