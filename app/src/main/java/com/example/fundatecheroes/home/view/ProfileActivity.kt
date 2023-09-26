7b74a89ea29455fb864afc233c5d74716b79faff
mairyeduardo <mairyeduardo@gmail.com>
2023-09-25 21:27:59 -0300

    alteracoes de id e mudanca de cores

--- app/src/main/java/com/example/fundatecheroes/home/view/ProfileActivity.kt
+++ app/src/main/java/com/example/fundatecheroes/home/view/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fundatecheroes.home.view
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fundatecheroes.R
 
@@ -10,5 +11,9 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         getSupportActionBar()?.hide()
         setContentView(R.layout.activity_profile)
+
+        findViewById<Button>(R.id.buttonCriarConta).setOnClickListener(){
+
+        }
     }
 }
