edf0136460467509196cbb95a50aedbfe19aba81
mairyeduardo <mairyeduardo@gmail.com>
2023-09-25 21:06:06 -0300

    tela profile, add imagens e inicio da logica de profile

--- app/src/main/java/com/example/fundatecheroes/home/view/ProfileActivity.kt
+++ app/src/main/java/com/example/fundatecheroes/home/view/ProfileActivity.kt
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.fundatecheroes.R
 
 class ProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getSupportActionBar()?.hide()
         setContentView(R.layout.activity_profile)
     }
 }
